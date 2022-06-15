ed071afcc15f4f94f6535101a32fede075ad1bed
melonwark <102952154+melonwark@users.noreply.github.com>
2022-06-15 16:29:24 +0300

    Create sum_square_diff.js

---
+++ sum_square_diff.js
@@ -0,0 +1,12 @@
+const squDif = (n) => {
+  let = resultSQ = 0;
+  let = resultSQ1 = 0;
+  let = acc = 0;
+  for (let i = 1; i <= n; i++) {
+    resultSQ = resultSQ + (i*i)
+    acc = acc + i
+    }
+  resultSQ1 = acc * acc;
+  return resultSQ1 - resultSQ;
+}
+console.log(squDif())
