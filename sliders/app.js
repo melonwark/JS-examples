5921f9b71e6850763c14512a80f311242acd1c6d
melonwark <102952154+melonwark@users.noreply.github.com>
2022-08-20 18:05:44 +0300

    Add files via upload

--- sliders/app.js
+++ sliders/app.js
@@ -1,1 +1,43 @@
+const dragEl = document.querySelector('.item')
+const placeholders=document.querySelectorAll('.placeholder')
+
+
+dragEl.addEventListener('dragstart', dragstart)
+dragEl.addEventListener('dragend', dragend)
+
+placeholders.forEach(el =>{
+    el.addEventListener('dragover',dragover)
+    el.addEventListener('dragenter', dragenter)
+    el.addEventListener('dragleave', dragleave)
+    el.addEventListener('drop', dragdrop)
+  
+})
+
+function dragstart (ev) {
+ev.target.classList.add('holded')
+setTimeout(()=>ev.target.classList.add('hide'),0)
+
+}
+
+function dragend (ev) {
+ev.target.classList.remove('holded','hide')
+}
+
+function dragover(ev){
+    ev.preventDefault()
+}
+
+function dragenter(ev){
+ev.target.classList.add('hover')
+}
+
+function dragleave(ev){
+ev.target.classList.remove('hover')
+}
+
+
+function dragdrop(ev){
+    ev.target.append(dragEl)
+    ev.target.classList.remove('hover')
+}
 
