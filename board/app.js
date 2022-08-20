d9a3bce1cd88a0db3b13f9fac4f8eae6213c4319
melonwark <102952154+melonwark@users.noreply.github.com>
2022-08-20 18:06:19 +0300

    Add files via upload

--- board/app.js
+++ board/app.js
@@ -1,1 +1,35 @@
+const board = document.querySelector('#board')
+const squareNumber= 500;
+const colors = ['red','green','blue']
 
+for (let i = 0; i <squareNumber;i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover',()=>
+    setColor(square))
+    
+    square.addEventListener('mouseleave',()=>
+    removeColor(square))
+    
+    board.append(square)  
+
+}
+
+function setColor(el) {
+    const color = getRandomCol()
+    el.style.backgroundColor = color
+    el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    
+}
+
+function removeColor(el){
+    el.style.backgroundColor = '#1d1d1d'
+    el.style.boxShadow = `0 0 2px #000`
+  
+}
+
+function getRandomCol() {
+   
+    return colors[Math.floor(Math.random() * colors.length)]
+}
